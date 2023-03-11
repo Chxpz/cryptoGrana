573894b8cc467e2403b84cff86941172abb62371
Chxpz <rpcweb3@gmail.com>
2023-03-10 22:08:34 -0300

    init flash loans

---
+++ blockchain/contracts/Brooker.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "./aave-contracts/interfaces/IFlashLoanSimpleReceiver.sol";
+import "hardhat/console.sol";
+
+interface IERC20{
+    function balanceOf(address account) external view returns (uint256);
+}
+
+interface IAAVEFlashLoan {
+
+    function flashLoanSimple(
+        address receiverAddress,
+        address asset,
+        uint256 amount,
+        bytes calldata params,
+        uint16 referralCode
+    ) external;
+}
+
+contract Broker is IFlashLoanSimpleReceiver {
+
+    function callFlashLoanSimple(
+        address poolAddress,
+        address asset,
+        uint256 amount,
+        bytes calldata params,
+        uint16 referralCode
+    ) external {
+        IAAVEFlashLoan aave = IAAVEFlashLoan(poolAddress);
+        aave.flashLoanSimple(
+            address(this),
+            asset,
+            amount,
+            params,
+            referralCode
+        );
+    }
+
+   function executeOperation(
+    address asset,
+    uint256 amount,
+    uint256 premium,
+    address initiator,
+    bytes calldata params
+  ) external returns (bool){
+    uint256 balance = IERC20(asset).balanceOf(address(this));
+    console.log("chamou aqui");
+    console.log(balance);
+    // do the logic
+  }
+
+  function ADDRESSES_PROVIDER() external view returns (IPoolAddressesProvider){
+
+  }
+
+  function POOL() external view returns (IPool){
+
+  }
+}
